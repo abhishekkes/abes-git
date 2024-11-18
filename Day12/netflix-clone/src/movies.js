6f04c6271931c1dc56b8e60555709d09e135c259
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2024-11-18 12:55:36 +0530

    Netflix  Clone

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,75 @@
+const movies = [
+  {
+    id: 1,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/5/p/7/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfevqrdeqzths.jpeg?q=70",
+    title: "TARZAN-THE WONDER CAR",
+    language: "Hindi",
+    cost: 250,
+  },
+  {
+    id: 2,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+    title: "Gupt",
+    language: "Hindi",
+    cost: 200,
+  },
+  {
+    id: 3,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+    title: "Mission Impossible",
+    language: "English",
+    cost: 500,
+  },
+  {
+    id: 4,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/k3dc7m80/movie/t/c/f/2018-blu-ray-universal-pictures-excel-home-videos-english-original-imafmggqgub6vreg.jpeg?q=70",
+    title: "Jurasic World 3",
+    language: "English",
+    cost: 600,
+  },
+  {
+    id: 5,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+    title: "MI 3",
+    language: "English",
+    cost: 800,
+  },
+  {
+    id: 6,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+    title: "Gupt 2",
+    language: "Hindi",
+    cost: 400,
+  },
+  {
+    id: 7,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+    title: "Gupt 3",
+    language: "Hindi",
+    cost: 600,
+  },
+  {
+    id: 8,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/j80icnk0/movie/8/c/w/2007-dvd-amutham-music-tamil-bala-padam-mirudangam-original-imaey4m5z3ghfsyw.jpeg?q=70",
+    title: "Bala Padam - Mirudangam",
+    language: "Hindi",
+    cost: 500,
+  },
+  {
+    id: 9,
+    image:
+      "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+    title: "Gupt",
+    language: "Hindi",
+    cost: 200,
+  },
+];
+export default movies;
